4fd9b7a2e4cd8d127bb95d7c4d1f3a81467a3e44
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-12-07 17:44:58 +0900

    study log

---
+++ study_note/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func doSomething(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done(): // 親からキャンセル通知を受け取る
+			fmt.Println("キャンセルされました:", ctx.Err())
+			return
+		default:
+			fmt.Println("処理中...")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func main() {
+	// キャンセル可能なコンテキストを作成
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// 子ゴルーチンを実行
+	go doSomething(ctx)
+
+	// 3秒後にキャンセル
+	time.Sleep(3 * time.Second)
+	cancel()
+
+	// キャンセル後も少し待機
+	time.Sleep(1 * time.Second)
+	fmt.Println("終了")
+}
